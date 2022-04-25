8cfa951011260d7fdb94d1dfc7d6418991bdb322
reid <reidbras@gmail.com>
2022-04-24 20:26:55 -0500

    UpdatingApp.js and App.css

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>This is my first React App!</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="App-paragraph">
+          React will make writing applications easier by having
+          a sigle page app that imports various components that
+          make up the page. Chill and listen to some music.
+          <br></br>
+          <br></br>
+          <iframe width="603" height="452" src="https://www.youtube.com/embed/8Yly0-wLigU" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </p>
         <a
           className="App-link"
